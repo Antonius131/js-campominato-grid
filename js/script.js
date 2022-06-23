e8ba276b81f16c640d28af7d6af587ccc9823d3e
Antonius131 <antomendetta@gmail.com>
2022-06-23 15:46:32 +0200

    create a cycle to put squares in the grid-wrapper

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
  * Aggiungere una griglia all'interno di un contenitore del DOM (#grid-wrapper)
  *    ogni cella della griglia conterrà un numero progressivo a 1 a 100 ==> griglia 10x10
  *    creare un ciclo che generi dinamicamente 100 celle quadrate   VVV
- *    creare una serie di numeri progressivi da 1 a 100
+ *    creare una serie di numeri progressivi da 1 a 100 e inserirli nella cella VVV
  * 
  *    inserire i numeri generati in ogni cella della griglia
  *    al click su una cella, questa si colorerà di azzurro
@@ -15,6 +15,13 @@
  *    
  */
 
+const gridWrapper = document.getElementById('grid-wrapper');
+
+for (let i = 0;  i <= 100;  i++) {
+   const square = squareGenerator();
+   square.innerHTML = i;
+
+}
 
 
 function squareGenerator() {
